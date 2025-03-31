1a3b83fabcb110e015e9a20024b9f9cecca084c8
millieloisi <167239633+millieloisi@users.noreply.github.com>
2025-03-31 12:05:14 -0300

    Datos del usuario terminado

---
+++ Assets/DatsoDelUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatsoDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float estaturaUsuario;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario + " , tengo " + edadUsuario
+            + " , mido" + estaturaUsuario + " metros y es " + esDonante + " que soy donante de organos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
