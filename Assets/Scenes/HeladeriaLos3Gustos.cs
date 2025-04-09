b7f5a9ca1ffb43faddadbaa2e1896ba660bdfb7d
millieloisi <167239633+millieloisi@users.noreply.github.com>
2025-04-09 15:55:37 -0300

    Heladeria Los 3 gustos

---
+++ Assets/Scenes/HeladeriaLos3Gustos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeladeriaLos3Gustos : MonoBehaviour
+{
+    //1. DEclaracion las Variables
+    public string codigoGusto; // public string es para que sea publico y para palabras, codigoGusto es el nombre de la variable
+    public int gramosHelado; // public string es para que sea publico y para numeros, codigoGusto es el nombre de la variable
+
+    float precioPorMilGramos; 
+    
+    void Start()
+    {
+        /// 2. Ingreso Y Validacion de Datos
+
+        if (gramosHelado < 250 || gramosHelado > 3000)
+        {
+            Debug.Log("Cantidad de helado no validad"); // Si no se cumple el if de mayor a 250g y menor a 3000g, con el Debug.Log devuelve el mesaje entre comillas adentro del parentisis
+            return;
+        }
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
